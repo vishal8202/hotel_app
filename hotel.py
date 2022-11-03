809bff5f7208a9e1a36e657575bbfca25542cfbe
vishal8202 <vishalvijayan222@gmail.com>
2022-11-03 13:32:38 +0530

    first

---
+++ hotel.py
@@ -0,0 +1,25 @@
+while(True):
+    print("\n please select an option")
+    print("1 Tea")
+    print("2 Coffee")
+    print("3 Buger")
+    print("4 Sandwich")
+    print("5 Alpham")
+    print("6 Generate Bill")
+    print("7 Exit")
+    ch=int(input("Enter the choice"))
+    
+    if(ch==1):
+        print("Added Tea")
+    elif(ch==2):
+        print("Added Coffee")
+    elif(ch==3):
+        print("Added Burger")
+    elif(ch==4):
+        print("Added Sandwich")
+    elif(ch==5):
+        print("Added lime")
+    elif(ch==6):
+        print("Generating bill")
+    elif(ch==7):
+        break
